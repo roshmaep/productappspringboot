6823936dcfa8e815557011df41f9440f5e50ed88
roshmaep <roshmaep1999@gmail.com>
2022-12-07 11:16:56 +0530

    status

--- src/main/java/com/example/productapp_backend/controller/ProductController.java
+++ src/main/java/com/example/productapp_backend/controller/ProductController.java
@@ -5,7 +5,9 @@ import com.example.productapp_backend.model.Products;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ProductController {
@@ -14,7 +16,7 @@ public class ProductController {
     @CrossOrigin(origins = "*")
 
     @PostMapping(path = "add",consumes = "application/json",produces = "application/json")
-    public String ProductAdd(@RequestBody Products p){
+    public Map<String,String> ProductAdd(@RequestBody Products p){
         System.out.println(p.getBrand().toString());
         System.out.println(p.getDistributor().toString());
         System.out.println(p.getExpdate().toString());
@@ -24,10 +26,12 @@ public class ProductController {
         System.out.println(p.getSellername().toString());
         System.out.println(p.getProductcode());
         dao.save(p);
-        return " product added successfully";
+        HashMap<String,String> map=new HashMap<>();
+        map.put("status","success");
+        return map;
     }
 @CrossOrigin(origins = "*")
-    @GetMapping("view")
+    @GetMapping("/view")
     public List <Products> ProductView()
     {
         return (List<Products>) dao.findAll();
